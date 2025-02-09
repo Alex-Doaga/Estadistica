555e8ea1c9be518437a81a115fc73f42d96ba738
doaga <doaga@alumni.uv.es>
2025-02-09 22:36:48 +0100

    Métodos fórmulas

---
+++ src/main/java/es/uji/alexandru/estadistica/Estadistica.java
@@ -0,0 +1,32 @@
+package es.uji.alexandru.estadistica;
+
+public class Estadistica {
+
+    //Método para calcular la media
+    public static double mediaAritmetica (double []v){
+        if(v.length == 0)
+            return 0;
+        double suma = 0;
+        for (double value : v) suma += value;
+
+        return suma/v.length;
+    }
+
+    //Método para calcular la varianza
+    public static double varianza (double []v){
+        if(v.length == 0)
+            return 0;
+        double suma = 0;
+        double media = mediaAritmetica(v);
+
+        for (double value : v){
+            suma += Math.pow(value - media, 2);
+        }
+        return suma / v.length;
+    }
+
+    //Método para calcular la desviación estándar
+    public static double desviacionEstandar (double []v){
+        return Math.sqrt(varianza(v));
+    }
+}
